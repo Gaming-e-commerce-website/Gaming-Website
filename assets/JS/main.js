8c5a536e49b7c201960315aeed1977b0eecf932f
Esraa Shalaby <esraashalaby686@gmail.com>
2026-04-20 21:13:06 +0200

    adding css js files

---
+++ assets/JS/main.js
@@ -0,0 +1,19 @@
+
+
+let nav = document.querySelector('#nav');
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 500) {
+        nav.classList.add('fixed');
+        nav.style.opacity = '1';
+
+    } 
+    else if (window.scrollY > 100 && window.scrollY <= 500) {
+        nav.style.opacity = '0';
+        nav.classList.remove('fixed');
+    }
+     else {
+        nav.style.opacity = '1';
+        nav.classList.remove('fixed');
+    }
+});
